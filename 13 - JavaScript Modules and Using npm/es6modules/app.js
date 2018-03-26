a0b67e159db654f83479563acaf32ef88cd1a48e
ach <ignaciomenchaca@gmail.com>
2018-03-26 00:14:11 -0300

    Progreso clase 13

---
+++ 13 - JavaScript Modules and Using npm/es6modules/app.js
@@ -0,0 +1,6 @@
+// Manejar reglas eslint desde el archivo
+
+/* eslint-disable linebreak-style, no-unused-vars */
+import { uniq } from 'lodash';
+import insane from 'insane';
+import jsonp from 'jsonp';
