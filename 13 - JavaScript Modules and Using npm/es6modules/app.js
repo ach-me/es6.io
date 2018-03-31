12dc6040e134808f0e17edb6d5c4248bd2eeaeed
ach <ignaciomenchaca@gmail.com>
2018-03-31 17:53:28 -0300

    Clase 46

--- 13 - JavaScript Modules and Using npm/es6modules/app.js
+++ 13 - JavaScript Modules and Using npm/es6modules/app.js
@@ -4,6 +4,16 @@
 import { uniq } from 'lodash'; // named export entre {}
 import insane from 'insane';
 import jsonp from 'jsonp';
+// Para acceder a la variable declarada en config.js hay que importarla
+// Como no es un modulo node, es un archivo, hay que indicar la ubicacion del archivo
+// Como se utilizo una exportacion default, se puede usar cualquier nombre al importarla
+import lalala from './src/config';
+// Named export
+import { key as otroNombre, url, sayHi, age, two } from './src/config';
+
+console.log(lalala);
+console.log(otroNombre);
+sayHi('Nac');
 
 const ages = [1, 1, 4, 52, 12, 4];
 
---
+++ 13 - JavaScript Modules and Using npm/es6modules/src/config.js
@@ -0,0 +1,24 @@
+const apiKey = 'Default Export';
+
+// Como hacer para acceder a esta variable desde otro archivo o modulo?
+// Para que esta variable esté disponible hay que exportalar. Hay dos maneras
+
+// 1) Export default
+// Cuando se importa, se puede usar cualquier nombre
+// Solo se puede declarar un solo export default
+export default apiKey;
+
+// 2) Named export
+// Al importala, se debe usar el mismo nombre con que se declaro
+// Se pueden utilizar multiples named export
+export const key = 'Named Export';
+export const url = 'http://www.google.com';
+export function sayHi(name) {
+  console.log(`Hello there ${name}`);
+}
+
+const one = 100;
+const two = 200;
+
+export { one as age, two };
+
